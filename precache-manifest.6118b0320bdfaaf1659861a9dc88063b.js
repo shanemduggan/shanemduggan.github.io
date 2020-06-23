c0f0ede1c9827cfa3e2c3dfb2a73b7ec62e97e8b
Shane Duggan <devshaneduggan@gmail.com>
2020-06-23 11:38:18 -0700

    Adds heresay 2.0 build, deletes heresay 1.0 build

---
+++ precache-manifest.6118b0320bdfaaf1659861a9dc88063b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c6acafac763f49637c1abc5a738b8b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d6773d868c35c191899",
+    "url": "./static/css/main.8ea4b54c.chunk.css"
+  },
+  {
+    "revision": "50b40f7157a84a5039e7",
+    "url": "./static/js/2.ad129ebb.chunk.js"
+  },
+  {
+    "revision": "3ef76f0a45c060e2fec7c12129166bf5",
+    "url": "./static/js/2.ad129ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d6773d868c35c191899",
+    "url": "./static/js/main.0def4d68.chunk.js"
+  },
+  {
+    "revision": "b3eb2a7d6701dfc6851a",
+    "url": "./static/js/runtime-main.8fa64397.js"
+  },
+  {
+    "revision": "7e5a40b114ff87c483e7501c1c2dc5f9",
+    "url": "./static/media/blue-dot.7e5a40b1.png"
+  },
+  {
+    "revision": "b8ae76b12bd3fe0fd25f3e6dc4604a26",
+    "url": "./static/media/green-dot.b8ae76b1.png"
+  },
+  {
+    "revision": "e38876e450c77c8bc19f4cbf0ee9eb3e",
+    "url": "./static/media/heart.e38876e4.png"
+  },
+  {
+    "revision": "fec517d0762c2e518edace07d5c9c220",
+    "url": "./static/media/orange-dot.fec517d0.png"
+  }
+]);
